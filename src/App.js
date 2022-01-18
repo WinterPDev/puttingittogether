0138a19d22fa7cb1b0bf3e028a64b7052d35469a
WinterP <winterp.dev@gmail.com>
2022-01-17 21:24:17 -0600

    Convert to BigInversion

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import PersonCard from './components/PersonCard';
 function App() {
   return (
     <div className="App">
-      <PersonCard firstName={"Jane"} lastName={"Doe"} age={45} hairColor={"Black"}/>
-      <PersonCard firstName={"Smith"} lastName={"John"} age={88} hairColor={"Brown"}/>
-      <PersonCard firstName={"Fillmore"} lastName={"Millard"} age={50} hairColor={"Violet"}/>
-      <PersonCard firstName={"Smith"} lastName={"Maria"} age={62} hairColor={"Teal"}/>
+      <PersonCard firstName="Jane" lastName="Doe" age={45} hairColor="Black"/>
+      <PersonCard firstName="Smith" lastName="John" age={88} hairColor="Brown"/>
+      <PersonCard firstName="Fillmore" lastName="Millard" age={50} hairColor="Violet"/>
+      <PersonCard firstName="Smith" lastName="Maria" age={62} hairColor="Teal"/>
     </div>
   );
 }
